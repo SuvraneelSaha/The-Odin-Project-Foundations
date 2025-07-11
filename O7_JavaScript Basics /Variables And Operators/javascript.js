62be8ab15ee55623a71b6dddd9154d8abe6a4ec2
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-07-11 10:49:06 +0530

    foundations

---
+++ O7_JavaScript Basics /Variables And Operators/javascript.js
@@ -0,0 +1,44 @@
+console.log("Hello nigga dont you wanna see your Potential?");
+
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname); 
+
+const pi = 3.14;
+
+console.log(pi); // What will be output?
+
+/*
+So in summary, there are two ways to declare a variable:
+
+let, which we can re-assign.
+const which we can’t re-assign and will throw an error if we try.
+
+*/
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 2 ; 
+console.log(a);
+a=9;
+console.log(a);
+let b = 7 * a;
+console.log(b);
+
+const max = 57 ; 
+const actual = max - 13 ; 
+const percentage = actual/max;
+console.log(percentage);
+
+// In other words, capital-named constants are only used as aliases for “hard-coded” values.
+
+ 
+/*
+In contrast, age is evaluated in run-time.
+ Today we have one age, a year after we’ll have another one.
+  It is constant in a sense that it does not change through the code execution.
+   But it is a bit “less of a constant” than birthday: it is calculated, 
+   so we should keep the lower case for it.
+*/
